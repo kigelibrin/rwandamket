1415a92097ba79789375266add9c25593daf76d7
Byamungu Gloire <gogobrinn@gmail.com>
2025-12-27 00:13:19 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+
+const supabaseUrl = 'https://bulxwiknhwafvfzodheb.supabase.co';
+const supabaseKey = 'sb_publishable_G5SGumxFBV1Ju2kFFYupXA_NNLyhL83';
+
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
